d1f8661889b4e1d8a2649b68b1c56578d4157bfe
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2023-10-12 08:30:25 -0300

    feat: categories api

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-console': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -8,6 +8,19 @@ class CategoryController {
     response.json(categories);
   }
 
+  async show(request, response) {
+    // Obter um registro
+    const { id } = request.params;
+    const category = await CategoryRepository.findById(id);
+
+    if (!category) {
+      response.status(404).json({ error: 'Category not found' });
+      return;
+    }
+
+    response.json(category);
+  }
+
   async store(request, response) {
     const { name } = request.body;
 
@@ -18,6 +31,40 @@ class CategoryController {
     const category = await CategoryRepository.create({ name });
 
     return response.json(category);
+  }
+
+  async update(request, response) {
+    // Editar novo registro
+    const { id } = request.params;
+    const {
+      name,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Require fields, name can not be empty ' });
+    }
+
+    const categoryExists = await CategoryRepository.findById(id);
+
+    if (!categoryExists) {
+      // 404: not found
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    const contact = await CategoryRepository.update(id, {
+      name,
+    });
+
+    return response.json(contact);
+  }
+
+  async delete(request, response) {
+    // Deletar um registro
+    const { id } = request.params;
+
+    await CategoryRepository.delete(id);
+    // 204: no content
+    response.sendStatus(204);
   }
 }
 
